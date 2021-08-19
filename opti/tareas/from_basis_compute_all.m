75a662e3a57f07a3c7e03940d56db2334c8506e3
DaveAlsina <58042964+DaveAlsina@users.noreply.github.com>
2021-08-19 13:40:50 -0500

    Update for sync from windows on 2021-08-19 1:40:50 PM .

--- opti/tareas/from_basis_compute_all.m
+++ opti/tareas/from_basis_compute_all.m
@@ -1,10 +1,22 @@
 function [] = from_basis_compute_all(basis, b, c, Ib, In)
 
+    %matriz basica
     B = basis(:, Ib);
+    %matriz no basica
     N = basis(:, In);
+    %matriz inversa de la base
     B_ = inv(B);
     
-    xb = B_ * b
-    Cj = c(:,In) - c(:,Ib)*B_*N 
-    N;
+    %soluciones basicas
+    xb = B_ * b;
+    
+    %vector de la solución
+    x(Ib) = xb;
+    x(In) = zeros(size(In));
+    
+    %costos reducidos
+    Cj = c(:,In) - c(:,Ib)*B_*N;
+    
+    %función objetivo
+    fo = c.*x
 end
--- opti/tareas/tarea1_main.m
+++ opti/tareas/tarea1_main.m
@@ -4,6 +4,7 @@ c = [-2 -1 0 0 0];
 Ib = [1 2 3];
 In = [4 5];
 
-from_basis_compute_all(A, b, c, Ib, In)
+from_basis_compute_all(A, b, c, Ib, In);
 
-inv([1 1 1; 1 0 0; 0 1 0])
+
+%inv([1 1 1; 1 0 0; 0 1 0])
