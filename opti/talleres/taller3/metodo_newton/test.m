4285fb119274362c635c2abcc0fb7562472aeb0b
DaveAlsina <58042964+DaveAlsina@users.noreply.github.com>
2021-09-16 07:04:00 -0500

    Update for sync from windows on 2021-09-16 7:03:57 AM .

---
+++ opti/talleres/taller3/metodo_newton/test.m
@@ -0,0 +1,12 @@
+%% ejemplo 1
+% https://www.mathworks.com/help/symbolic/differentiation.html 
+
+syms x
+f = (x.^2)./10 - 2.*sin(x);
+x0 = 2.5;
+e = 0.0001;
+interval = [-100, 100];
+
+j = metodo_newton(f, x, e, x0, interval, true)
+
+%% ejemplo 2
