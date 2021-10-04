2167eb8cb861e3886fd6d7bab2bcc039f98a2051
DaveAlsina <58042964+DaveAlsina@users.noreply.github.com>
2021-10-04 07:02:19 -0500

    Update for sync from windows on 2021-10-04 7:02:18 AM .

--- opti/talleres/taller3/versiones_mejoradas/fit_cuadratico/test.m
+++ opti/talleres/taller3/versiones_mejoradas/fit_cuadratico/test.m
@@ -15,3 +15,11 @@ interval = [-1 4];
 i = 0.001;
 
 quad_fit_version_clase(f, [-1 4], [0 1 4], i);
+
+
+%% 
+f = @(x) x.^3 - x.^2 -x;             
+interval = [0 4];
+i = 0.001;
+
+quad_fit_version_clase(f, interval, [0 2 4], i, 1);
--- opti/talleres/taller4_progra/descenso_grad/main_gradientDescent.m
+++ opti/talleres/taller4_progra/descenso_grad/main_gradientDescent.m
@@ -14,6 +14,17 @@ ng = norm(g);
 gradientDescent(x, f, xstart, epsilon, interval, 100, verbose);
 
 %%
+syms x [1 2] real           %variables simbólicas x1 y x2 empaquetadas en x
+f(x1, x2) = x1 - x2 + 2.*(x1.^2) + 2.*(x1.*x2) + x2.^2;     %función simbólica
+xstart = [0;0];
+epsilon = 0.5;
+interval = [-1000, 1000];
+verbose = 1;
+
+%%
+
+gradientDescent(x, f, xstart, epsilon, interval, 100, verbose);
+%%
 
 % syms: 
 %https://www.mathworks.com/help/symbolic/sym.html#mw_b4228c40-a002-49a8-a6cc-1bec4af17ca8
